ca65154580a5fb39e04e4a19fd71478534571d31
liubinbin <lotosbin@gmail.com>
2016-07-20 14:40:30 +0800

    .UseKestrel() .UseUrls("http://localhost:10571/")

--- src/Love.Net.Help.Host/Program.cs
+++ src/Love.Net.Help.Host/Program.cs
@@ -14,6 +14,7 @@ namespace Love.Net.Help.Host
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://localhost:10571/")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
